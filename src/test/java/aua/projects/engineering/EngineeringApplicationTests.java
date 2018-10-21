d1b30811dc621c4acef13a46e77396f7d1b3eae3
hrach33 <hrachkhachatryan96@gmail.com>
2018-10-22 03:37:44 +0400

    unit test

--- src/test/java/aua/projects/engineering/EngineeringApplicationTests.java
+++ src/test/java/aua/projects/engineering/EngineeringApplicationTests.java
@@ -1,17 +1,28 @@
 package aua.projects.engineering;
 
+import aua.projects.engineering.dao.EmergencyDao;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static junit.framework.TestCase.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class EngineeringApplicationTests {
 
+    @Autowired
+    private EmergencyDao emergencyDao;
 
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void testDao(){
+        assertTrue(emergencyDao.getAllUsers() != null);
     }
 
 }
